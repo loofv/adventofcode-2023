10d4357ddae7aa891aba3eab077e7a6d519c0b81
Love Hulterström <love.hulterstrom@gmail.com>
2022-12-01 23:50:23 +0100

    working solution 2

--- 01/01.py
+++ 01/01.py
@@ -2,23 +2,45 @@ f = open("input.txt", "r")
 
 lines = f.read().splitlines()
 
+elfKcals = []
+
 maxKcal = 0
-currKcal = 0
+currElf = 0
 for line in lines:
-    print(line)
     if line != '':
-        print("was not empty string")
-        currKcal += int(line)
-        print("currKcal is now: ", currKcal)
+        currElf += int(line)
     else:
-        print("was empty string")
-        print("maxkcal is: ", maxKcal)
-        if currKcal > maxKcal:
-            print("curr was greater than max")
-            print("curr is: ", currKcal)
-            print("max is: ", maxKcal)
-            maxKcal = currKcal
-            print("max is now set to: ", maxKcal)
-        currKcal = 0
-print("expected: ", 24000)
-print("actual: " , maxKcal)
+        elfKcals.append(currElf)
+        currElf = 0
+
+print(elfKcals)
+elfKcals.sort(reverse=True)
+print(elfKcals)
+
+top3Total = elfKcals[0] + elfKcals[1] + elfKcals[2]
+
+print("top 3 total: ", top3Total)
+# print("expected: ", 24000)
+# print("actual: " , maxKcal)
+
+
+
+
+# for line in lines:
+#     if line != '':
+#         print("was not empty string")
+#         currElf.add(int(line))
+#         currKcal += int(line)
+#         print("currKcal is now: ", currKcal)
+#     else:
+#         print("was empty string")
+#         print("maxkcal is: ", maxKcal)
+#         if currKcal > maxKcal:
+#             print("curr was greater than max")
+#             print("curr is: ", currKcal)
+#             print("max is: ", maxKcal)
+#             maxKcal = currKcal
+#             print("max is now set to: ", maxKcal)
+#         currKcal = 0
+# print("expected: ", 24000)
+# print("actual: " , maxKcal)
