7476738b7a2fc35e9b644c69f2272fd005e13c13
Love Hulterström <love.hulterstrom@gmail.com>
2022-12-01 23:38:04 +0100

    first half working solution

--- 01/01.py
+++ 01/01.py
@@ -1,1 +1,24 @@
-print("hello world!")
+f = open("input.txt", "r")
+
+lines = f.read().splitlines()
+
+maxKcal = 0
+currKcal = 0
+for line in lines:
+    print(line)
+    if line != '':
+        print("was not empty string")
+        currKcal += int(line)
+        print("currKcal is now: ", currKcal)
+    else:
+        print("was empty string")
+        print("maxkcal is: ", maxKcal)
+        if currKcal > maxKcal:
+            print("curr was greater than max")
+            print("curr is: ", currKcal)
+            print("max is: ", maxKcal)
+            maxKcal = currKcal
+            print("max is now set to: ", maxKcal)
+        currKcal = 0
+print("expected: ", 24000)
+print("actual: " , maxKcal)
